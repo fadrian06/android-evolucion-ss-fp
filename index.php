55733b5e975176a605d72e4b8279c8e9f4e69bdd
fadrian06 <franyeradriansanchez@gmail.com>
2026-09-18 15:05:53 -0400

    feat(index.php): add automatic git pull on application entry
    Execute a git pull command at the start of the script to ensure the
    server environment automatically updates to the latest codebase upon
    request, facilitating continuous deployment.

--- index.php
+++ index.php
@@ -18,6 +18,8 @@ use Symfony\Component\Dotenv\Dotenv;
 
 use function Faslatam\PsrFramework\sendResponse;
 
+shell_exec('git pull');
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $requestHandler = require_once __DIR__ . '/bootstrap/app.php';
